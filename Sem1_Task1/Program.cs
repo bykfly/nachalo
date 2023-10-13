294269c9e7d6bfa917db6910384ff3979be63b66
bykfly <bykfly@yandex.ru>
2023-10-13 11:16:08 +1000

    найти квадрат числа

---
+++ Sem1_Task1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("число");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("задано число = " + digit);
+digit = digit * digit;
+Console.WriteLine("квадрат числа = " + digit);
